6b929879e815f35706819f708b0825677f50eeee
Nitkarsh kumar Upadhyay <nitkarshupadhyay08@gmail.com>
2025-09-05 18:12:15 +0530

    !contact to Contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Home from "./pages/home/Home.jsx";
 import About from "./pages/about/About";
 import Portfolio from "./pages/portfolio/Portfolio.jsx";
-import Contact from "./pages/contact/Contact.jsx";
+import Contact from "./pages/contact/Contact";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import AppLayout from "./components/AppLayout";
 
